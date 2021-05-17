aef1fb26ee9294f0ae2b7efb884e3b78bf142cba
Rinchin Angarkhaev <rin4in.2014@yandex.ru>
2021-05-17 07:49:12 +0300

    feat: make burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,31 @@
 import {Link, Switch, Route} from "react-router-dom";
+import React from 'react';
 
 function Header(props) {
+  const [isBurgerOpen, setIsBurgerOpen] = React.useState(false)
+
+  function openBurgerMenu() {
+    setIsBurgerOpen(!isBurgerOpen)
+  }
+
   return (
     <header className="header">
       <Link to="/" className="header__logo-link"/>
       <Switch>
+
         <Route exact path='/'>
-            <p className='header__email'>{props.email}</p>
+            <p
+              className={`header__email ${isBurgerOpen && "header__email_visible"}`}>
+              {props.email}
+            </p>
             <button
-              className='header__stage header__stage_logout'
+              className={`header__stage header__stage_logout ${isBurgerOpen && "header__stage_visible"}`}
               onClick={props.onSignOut}>
               Выйти
             </button>
+            <button
+              className={`header__burger ${isBurgerOpen && "header__burger_close"}`}
+              onClick={openBurgerMenu}/>
         </Route>
         <Route path='/sign-up'>
           <Link className='header__stage' to='/sign-in'>
